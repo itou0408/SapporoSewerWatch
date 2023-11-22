330354a93d01e2ae100a056fe98f373f45f780f5
jsditou <jsd.itou@gmail.com>
2023-11-22 12:35:14 +0900

    イメージの日付出力追加

--- インフルエンザ下水サーベイランスイメージDL.py
+++ インフルエンザ下水サーベイランスイメージDL.py
@@ -13,13 +13,14 @@ soup = BeautifulSoup(response.content, 'html.parser')
 image_tag = soup.find('img', src=re.compile(r'graph\d+\-i.jpg$'))
 src = image_tag['src']
 image_url = "https://www.city.sapporo.jp" + image_tag['src']
+image_date = re.search(r'graph(\d+)\-i.jpg$', src).group(1)
 
 
 # Channel (技術管理/通知)
 uri = "https://jsdazure.webhook.office.com/webhookb2/5e899d5d-9748-40ad-bbd1-72b37bda771f@f432b760-3333-434b-801a-26e24f55c413/IncomingWebhook/27edbb3dfddc4f3aa4f6a2be11da1158/0f569ab7-e490-441d-b11d-4776ab6a5bee"
 
 message = "札幌市のインフルエンザ下水サーベイランスの結果です。"
-message_with_imageurl = f"{message}\n![サーベイランス結果]({image_url})"
+message_with_imageurl = f"{message}({image_date})\n![サーベイランス結果]({image_url})"
 
 # ペイロードの作成
 payload = {
